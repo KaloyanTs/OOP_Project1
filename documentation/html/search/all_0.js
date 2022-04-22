74f0c8deda6454dc6589230c59c3d791be2983e4
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2022-04-22 20:57:03 +0300

    Doxygen generated manual

---
+++ documentation/html/search/all_0.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['guest_0',['Guest',['../class_guest.html',1,'']]]
+];
---
+++ documentation/html/search/searchdata.js
@@ -0,0 +1,18 @@
+var indexSectionsWithContent =
+{
+  0: "gr",
+  1: "gr"
+};
+
+var indexSectionNames =
+{
+  0: "all",
+  1: "classes"
+};
+
+var indexSectionLabels =
+{
+  0: "All",
+  1: "Classes"
+};
+
