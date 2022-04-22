f09bd7596a806be00e824cb7e8f2a5eea68dd96e
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2022-04-23 02:24:47 +0300

    implementation of free and accomodate (Room)

--- Room.cpp
+++ Room.cpp
@@ -4,3 +4,17 @@ std::ostream &operator<<(std::ostream &os, const Room &R)
 {
     return os << R.getNumber() << '\t' << R.getBedCount();
 }
+
+bool Room::accomodateHere()
+{
+    if (!free)
+        return false;
+    return !(free = false);
+}
+
+bool Room::freeRoom()
+{
+    if (free)
+        return false;
+    return !(free = true);
+}
