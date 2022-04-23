2c09b9011bb8e285207934ad1f75e01b1741f614
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2022-04-23 11:47:56 +0300

    testing Date

--- main.cpp
+++ main.cpp
@@ -1,8 +1,76 @@
 #include <iostream>
 #include "Reservation.hpp"
+#include "Date.hpp"
 
 int main()
 {
-
+    Date d = Date::getToday();
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
+    std::cout << ++d << '\n';
     return 0;
 }
