79643ed40b8d44b5522c45c827784599337f716f
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2022-04-23 20:59:39 +0300

    comments for the documentation

--- Room.hpp
+++ Room.hpp
@@ -3,36 +3,105 @@
 #include <iostream>
 #include "Types.hpp"
 #include "Reservation.hpp"
+
+/**
+ * @brief const varieble keeping minimal size of the reservations list
+ *
+ */
 const size_t INIT_CAPACITY = 2;
 
 class Room
 {
+    /**
+     * @brief ID of the room
+     *
+     */
     unsigned number;
+    /**
+     * @brief number of beds in the Room
+     *
+     */
     unsigned bedCount;
+    /**
+     * @brief list of reservations assigned to the Room
+     *
+     */
     Reservation **reservations;
+    /**
+     * @brief size and capacity of the reservations list
+     *
+     */
     size_t resCount, resCapacity;
 
+    /**
+     * @brief expand the reservations list
+     *
+     */
     void expand();
+    /**
+     * @brief shrink the reservations list
+     *
+     */
     void shrink();
 
 public:
+    /**
+     * @brief Construct a new Room object
+     *
+     * @param n number of constructed Room
+     * @param bC number of beds in constructed Room
+     */
     Room(unsigned n, unsigned bC);
+    /**
+     * @brief forbidden copying of rooms
+     *
+     */
     Room(const Room &) = delete;
+    /**
+     * @brief forbidden copying of rooms
+     *
+     */
     Room &operator=(const Room &) = delete;
+    /**
+     * @brief Destroy the Room object
+     *
+     */
     ~Room();
 
     unsigned getNumber() const { return number; }
     unsigned getBedCount() const { return bedCount; }
     bool isFreeNow() const;
 
-    bool accomodateHere(const Reservation &res);
+    /**
+     * @brief try to free this room
+     *
+     * @return true sucesfully freed room
+     * @return false room is already free
+     */
     bool freeRoom();
 
+    /**
+     * @brief apply new Date to state of all reservations and respectively of the room availability
+     *
+     */
     void newDate(Date);
 
+    /**
+     * @brief see if this room is free in certain date
+     *
+     * @return true the room is free
+     * @return false the room is taken
+     */
     bool isFreeOnDate(Date) const;
 };
 
+/**
+ * @brief prints Room's base info (number and bedCount)
+ *
+ * @param os output stream
+ * @param R Room to be printed
+ * @return std::ostream& same output stream
+ */
 std::ostream &operator<<(std::ostream &os, const Room &R);
 
 #endif
