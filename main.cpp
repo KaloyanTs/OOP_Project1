1559b3e300f15aa93551a7a6fba6a8fcbb067cbf
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2022-04-24 11:50:06 +0300

    testing difference of dates

--- main.cpp
+++ main.cpp
@@ -5,6 +5,7 @@
 int main()
 {
     Date d = Date::getToday();
+    std::cout << Date(1, 5, 2022)-d << '\n';
     std::cout << ++d << '\n';
     std::cout << ++d << '\n';
     std::cout << ++d << '\n';
