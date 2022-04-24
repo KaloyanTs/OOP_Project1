3c1ea15a1627ece4a9ea49dc6022c8dac66968aa
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2022-04-24 20:09:25 +0300

    reserve service and free room written

--- Hotel.cpp
+++ Hotel.cpp
@@ -19,3 +19,79 @@ Hotel &Hotel::getReport(Date from, Date to)
     building->createReport(from, to);
     return *this;
 }
+
+bool Hotel::reserveRoom(unsigned number, Date from, Date to, std::string name, std::string s)
+{
+    if (!((*building)[number]))
+    {
+        std::cerr << "No such Room.\n";
+        return false;
+    }
+    if ((*building)[number]->addReservation(name, s, from, to))
+    {
+        std::clog << "Reservation successfully made!\n";
+        return true;
+    }
+    std::cerr << "Room is reserved for this period.\n";
+    return false;
+}
+
+Hotel::Hotel(const char *hotelDataFile)
+{
+
+    std::ifstream ifs(hotelDataFile, std::ios::in);
+    size_t length;
+    ifs >> length;
+    ifs.get();
+    name = readFromIfstream(ifs, length);
+    building = new HotelBuilding(ifs);
+    ifs.close();
+}
+
+std::string readFromIfstream(std::ifstream &ifs, size_t len)
+{
+    char *buf = new char[len + 1];
+    ifs.getline(buf, len + 1);
+    buf[len] = '\0';
+    std::string res(buf);
+    delete[] buf;
+    return res;
+}
+
+Hotel::~Hotel()
+{
+    delete building;
+}
+
+bool Hotel::serviceRoom(unsigned number, Date from, Date to, std::string note)
+{
+    if (!((*building)[number]))
+    {
+        std::cerr << "No such Room.\n";
+        return false;
+    }
+    if ((*building)[number]->closeForService(note, from, to))
+    {
+        std::clog << "Service successfully planned!\n";
+        return true;
+    }
+    std::cerr << "Room is reserved for this period.\n";
+    return false;
+}
+
+bool Hotel::freeRoom(unsigned number)
+{
+    Reservation *tmp;
+    if (!((*building)[number]))
+    {
+        std::cerr << "No such room.\n";
+        return false;
+    }
+    if ((*building)[number]->freeRoom(tmp))
+    {
+        std::clog << "Room freed successfully.\n";
+        return true;
+    }
+    std::cerr << "Room is free already!\n";
+    return true;
+}
--- Hotel.hpp
+++ Hotel.hpp
@@ -5,25 +5,35 @@
 #include "Room.hpp"
 #include "Reservation.hpp"
 #include "HotelBuilding.hpp"
+#include <string>
+
+std::string readFromIfstream(std::ifstream &ifs, size_t len);
 
 class Hotel
 {
-    char *name;
+    std::string name;
     static Date now;
-    bool init;
     HotelBuilding *building;
 
     static Date getToday();
 
 public:
+    Hotel() = delete;
     Hotel(const char *hotelDataFile);
+    Hotel(const Hotel &) = delete;
+    Hotel &operator=(const Hotel &) = delete;
+    ~Hotel();
 
     static Date today() { return now; }
-    const char *getName() const { return name; }
+    std::string getName() const { return name; }
 
     void nextDay();
+    bool reserveRoom(unsigned number, Date from, Date to, std::string name = "-", std::string s = "None\n");
     Hotel &showAvailableRooms(std::ostream &, Date);
+    bool freeRoom(unsigned number);
     Hotel &getReport(Date from, Date to);
+    unsigned searchRoom(unsigned minBeds) const;
+    bool serviceRoom(unsigned number, Date from, Date to, std::string note);
 };
 
 #endif
