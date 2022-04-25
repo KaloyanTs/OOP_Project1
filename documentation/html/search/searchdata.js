bab3962719061c88bb7a36f34ce0b83c3e9a168b
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2022-04-26 01:20:29 +0300

    Doxygen generated documentation

--- documentation/html/search/searchdata.js
+++ documentation/html/search/searchdata.js
@@ -1,24 +1,12 @@
 var indexSectionsWithContent =
 {
-  0: "dfghinorst~",
-  1: "dhr",
-  2: "dfghinorst~",
-  3: "o"
 };
 
 var indexSectionNames =
 {
-  0: "all",
-  1: "classes",
-  2: "functions",
-  3: "related"
 };
 
 var indexSectionLabels =
 {
-  0: "All",
-  1: "Classes",
-  2: "Functions",
-  3: "Friends"
 };
 
