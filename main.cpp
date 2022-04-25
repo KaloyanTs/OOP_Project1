23c746d7dd7d01405d45f37f338e873136fd3764
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2022-04-25 13:10:51 +0300

    beginning of console interface

--- main.cpp
+++ main.cpp
@@ -1,9 +1,30 @@
 #include <iostream>
+#include <cstring>
 #include <string>
 #include "Hotel.hpp"
+
+bool workDay(Hotel &H)
+{
+    char cmd[100];
+    std::cin.getline(cmd, 100);
+    if (!strcmp(cmd, "close"))
+        return false;
+    if (!strcmp(cmd, "end day"))
+    {
+        H.nextDay();
+        return true;
+    }
+    return false;
+}
 
 int main()
 {
     Hotel H("h.history");
+    std::cout << "<<< " << H.getName() << " SYSTEM >>>\n";
+    do
+    {
+        std::cout << "Today is " << Hotel::today() << ".\n";
+    } while (workDay(H));
+
     return 0;
 }
