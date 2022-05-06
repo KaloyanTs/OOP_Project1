cacf7a3a39b78067d746ad3b17036b2f4b673db9
Turist5 <kaloyanyuliyanovtsvetkov02@gmail.com>
2022-05-06 12:08:03 +0300

    constant for length of strings

--- Constants.hpp
+++ Constants.hpp
@@ -12,6 +12,8 @@ const size_t DISPLAY_WIDTH = 130;
 
 const size_t COMMANDS = 8;
 
+const size_t STRING_MAX_LENGTH = 128;
+
 /**
  * @brief const variable keeping minimal size of the reservations list
  *
@@ -23,5 +25,26 @@ const size_t INIT_CAPACITY = 2;
  *
  */
 const size_t DISPLAY = 5;
+
+/**
+ * @brief
+ *
+ */
+const char cmdArr[COMMANDS][2][STRING_MAX_LENGTH] = {
+    {{"To make a reservation, enter "},
+     {"<reserve: [Room number] [Accomodation date] [Departure date] {Guest name[;]} {Note}>"}},
+    {{"To see list of free rooms for a particular date, enter "},
+     {"<available: [date]>"}},
+    {{"To free a room now, enter "},
+     {"<free: [Room number]>"}},
+    {{"To get report about the reservations of a room over a period of time, enter "},
+     {"<report: [From date] [To date]>"}},
+    {{"To request a room for guests, enter"},
+     {"<request: [minimal number of beds] [Accomodation date] [Departure date]>"}},
+    {{"To close a room for maintenance, enter"},
+     {"<maintenance: [room number] [From date] [To date] [Note]>"}},
+    {{"To see activity of all rooms, enter "}, {"<rooms:>"}},
+    {{"To see soonest date a room is free for some nights, enter "},
+     {"<plan: [Room number] [Number of nights]>"}}};
 
 #endif
